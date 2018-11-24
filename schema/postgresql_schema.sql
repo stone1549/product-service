e5f23e110b8b816c0de14b72c69dceec39ec1a07
Stone <stone1549@gmail.com>
2018-11-23 20:38:49 -0500

    Added indexes to product table.

--- schema/postgresql_schema.sql
+++ schema/postgresql_schema.sql
@@ -1,10 +1,13 @@
 CREATE TABLE product (
- id text PRIMARY KEY,
- name text NOT NULL,
- display_image text,
- thumbnail text,
- price numeric(15,6) NOT NULL,
- description text,
- short_description text,
- qty_in_stock int NOT NULL DEFAULT 0
+  id text PRIMARY KEY,
+  name text NOT NULL,
+  display_image text,
+  thumbnail text,
+  price numeric(15,6) NOT NULL,
+  description text,
+  short_description text,
+  qty_in_stock int NOT NULL DEFAULT 0
 );
+
+CREATE INDEX product_name_idx ON product (name);
+CREATE INDEX product_price_idx ON product (price);
