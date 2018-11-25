f4b7318c3d95757b8f7cd13462bbd7d1793c3548
Stone <stone1549@gmail.com>
2018-11-24 22:46:13 -0500

    Added SearchProducts implementation to in memory repo. Added bleve full text search lib as dependency. Added entry to .gitignore to ignore generated bleve index files.

--- repository/in_memory.go
+++ repository/in_memory.go
@@ -3,12 +3,15 @@ package repository
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"github.com/blevesearch/bleve"
 	"github.com/stone1549/product-service/common"
 	"io/ioutil"
 )
 
 type inMemoryProductRepository struct {
 	products []common.Product
+	index    bleve.Index
 }
 
 func (impr *inMemoryProductRepository) ProductsFromRepo(_ context.Context, first int, cursor string) (ProductList, error) {
@@ -22,7 +25,6 @@ func (impr *inMemoryProductRepository) ProductsFromRepo(_ context.Context, first
 }
 
 func findProductById(products []common.Product, id string) (*common.Product, error) {
-
 	for _, product := range products {
 		if id == product.Id {
 			return &product, nil
@@ -38,12 +40,48 @@ func (impr *inMemoryProductRepository) ProductFromRepo(_ context.Context, id str
 
 func (impr *inMemoryProductRepository) SearchProducts(ctx context.Context, searchTxt string, first int,
 	cursor string) (ProductList, error) {
-	return ProductList{}, newErrRepository("Search not implemented for in memory repo")
+	query := bleve.NewMatchQuery(searchTxt)
+	search := bleve.NewSearchRequest(query)
+	searchResults, err := impr.index.Search(search)
+
+	if err != nil {
+		return ProductList{}, err
+	}
+
+	products := make([]common.Product, 0)
+	var newCursor string
+	for _, hit := range searchResults.Hits {
+		if len(products) == first {
+			break
+		} else if hit.ID == cursor || cursor == "" || len(products) > 1 {
+			product, err := findProductById(impr.products, hit.ID)
+
+			if err != nil {
+				return ProductList{}, err
+			}
+			products = append(products, *product)
+			newCursor = product.Id
+		}
+	}
+
+	return ProductList{products, newCursor}, nil
 }
 
 func makeInMemoryRepository(config common.Configuration) (ProductRepository, error) {
 	var products []common.Product
 	var err error
+
+	// open a new index
+	mapping := bleve.NewIndexMapping()
+	idx, err := bleve.New("document", mapping)
+
+	if err == bleve.ErrorIndexPathExists {
+		idx, err = bleve.Open("document")
+	}
+
+	if err != nil {
+		return nil, err
+	}
 
 	switch config.GetInitDataSet() {
 	case common.NoDataset:
@@ -54,7 +92,26 @@ func makeInMemoryRepository(config common.Configuration) (ProductRepository, err
 		err = newErrRepository("Unsupported dataset %s for repo type PostgreSQL")
 	}
 
-	return &inMemoryProductRepository{products}, err
+	for _, product := range products {
+		var shortDescription string
+		if product.ShortDescription != nil {
+			shortDescription = *product.ShortDescription
+		}
+		var description string
+		if product.Description != nil {
+			description = *product.Description
+		}
+		// index name, id, short description, and full description
+		idxData := fmt.Sprintf("%s %s %s %s", product.Name, product.Id, shortDescription,
+			description)
+		err = idx.Index(product.Id, idxData)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &inMemoryProductRepository{products, idx}, err
 }
 
 func loadInitInMemoryDataset(dataset common.InitDataset) ([]common.Product, error) {
