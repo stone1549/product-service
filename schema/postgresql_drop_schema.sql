4ca245b3e6d6058cb638e1ff323cf641708195e2
Stone <stone1549@gmail.com>
2018-11-25 23:44:17 -0500

    Added created_at and updated_at fields to product table.

--- schema/postgresql_drop_schema.sql
+++ schema/postgresql_drop_schema.sql
@@ -1,8 +1,11 @@
+DROP INDEX product_created_at_idx;
+DROP INDEX product_updated_at_idx;
 DROP INDEX product_name_idx;
 DROP INDEX product_price_idx;
 DROP INDEX product_textsearch_idx;
 
 DROP TRIGGER product_search_update_trg ON product;
 DROP FUNCTION product_search_update_func();
-
+DROP TRIGGER product_set_updated_at_trg ON product;
+DROP FUNCTION set_updated_at();
 DROP TABLE product;
--- schema/postgresql_schema.sql
+++ schema/postgresql_schema.sql
@@ -7,12 +7,16 @@ CREATE TABLE product (
   description text,
   short_description text,
   qty_in_stock int NOT NULL DEFAULT 0,
+  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'UTC'),
+  updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'UTC'),
   textsearchable_index_col tsvector
 );
 
 CREATE INDEX product_name_idx ON product (name);
 CREATE INDEX product_price_idx ON product (price);
 CREATE INDEX product_textsearch_idx ON product USING GIN (textsearchable_index_col);
+CREATE INDEX product_created_at_idx ON product (created_at);
+CREATE INDEX product_updated_at_idx ON product (updated_at);
 
 CREATE FUNCTION product_search_update_func() RETURNS trigger AS $$
 begin
@@ -27,3 +31,17 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER product_search_update_trg BEFORE INSERT OR UPDATE
   ON product FOR EACH ROW EXECUTE PROCEDURE product_search_update_func();
+
+
+CREATE FUNCTION set_updated_at()
+  RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER product_set_updated_at_trg
+  BEFORE UPDATE ON product
+  FOR EACH ROW
+EXECUTE PROCEDURE set_updated_at();
